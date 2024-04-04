ba400c27ab63f78f5239bfa136511ec1d01353fe
Abdo Mo <enay9911@gmail.com>
2024-04-04 03:25:26 +0200

    Connecting to database & Ingesting data

--- src/data/scripts/data_ingestion.py
+++ src/data/scripts/data_ingestion.py
@@ -1,64 +1,17 @@
 
-
 import os
 from datetime import datetime
 from pymongo import MongoClient
 import pandas as pd
+from src.utils.database_utils import connect_to_database
 
 # MongoDB connection details
 MONGO_URI = "mongodb+srv://graduationproject993:Graduation2024@graduationproject.xkmscj5.mongodb.net/xfit?retryWrites=true&w=majority"
-DATABASE_NAME = "your_database_name"
-COLLECTION_NAME = "your_collection_name"
+DATABASE_NAME = "xfit"
+COLLECTION_NAME = "users"
 
 # Local directory for saving data
-DATA_DIR = "data/raw"
-
-def ingest_data():
-    """
-    Ingests data from an Atlas MongoDB database and saves it to a local directory.
-    """
-    # Create the data directory if it doesn't exist
-    os.makedirs(DATA_DIR, exist_ok=True)
-
-    # Connect to the MongoDB database
-    client = MongoClient(MONGO_URI)
-    db = client[DATABASE_NAME]
-    collection = db[COLLECTION_NAME]
-
-    # Fetch data from the MongoDB collection
-    data = list(collection.find())
-
-    # Convert the data to a Pandas DataFrame
-    df = pd.DataFrame(data)
-
-    # Save the data to a local CSV file
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_path = os.path.join(DATA_DIR, f"data_{timestamp}.csv")
-    df.to_csv(file_path, index=False)
-
-    print(f"Data ingested and saved to {file_path}")
-
-if __name__ == "__main__":
-    ingest_data()
-    
-    
-    
-
-# OR we can use the following code based on the database_utils file that we made
-
-
-import os
-from datetime import datetime
-import pandas as pd
-from utils.database_utils import connect_to_database
-
-# MongoDB connection details
-MONGO_URI = "mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<database>?retryWrites=true&w=majority"
-DATABASE_NAME = "your_database_name"
-COLLECTION_NAME = "your_collection_name"
-
-# Local directory for saving data
-DATA_DIR = "data/raw"
+DATA_DIR = "src/data/raw"
 
 def ingest_data():
     """
